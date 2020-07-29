f13297f1bf9378dc73245f3836db904668c73890
UnicornsOnLSD <44349936+UnicornsOnLSD@users.noreply.github.com>
2020-07-29 17:17:23 +0100

    Created an (incredibly janky) image uploader system

--- lib/screens/UploadingScreen.dart
+++ lib/screens/UploadingScreen.dart
@@ -1,26 +1,72 @@
 import 'dart:io';
+import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:imgur/imgur.dart' as imgur;
 
 import '../models/ImagePickerProvider.dart';
+import '../components/getImgurApiKey.dart';
 
-class UploadingScreen extends StatelessWidget {
+class UploadingScreen extends StatefulWidget {
   const UploadingScreen({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  _UploadingScreenState createState() => _UploadingScreenState();
+}
+
+class _UploadingScreenState extends State<UploadingScreen> {
+  Future uploadImageFuture;
+
+  Future<imgur.Image> _uploadImageFromProvider() async {
     ImagePickerProvider imagePickerProvider =
         Provider.of<ImagePickerProvider>(context, listen: false);
 
+    Uint8List imageBinary = await imagePickerProvider.chosenImage.readAsBytes();
+    String imageBase64 = base64Encode(imageBinary);
+
+    var imgurClient =
+        imgur.Imgur(imgur.Authentication.fromClientId(getImgurApiKey()));
+
+    imgur.Image uploadedImage =
+        await imgurClient.image.uploadImage(imageBase64: imageBase64);
+
+    return uploadedImage;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    uploadImageFuture = _uploadImageFromProvider();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Image.file(File(imagePickerProvider.chosenImage.path))],
-        ),
-      ),
+          child: FutureBuilder(
+              future: uploadImageFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("Uploading Image..."),
+                      Padding(padding: EdgeInsets.symmetric(vertical: 16)),
+                      CircularProgressIndicator(),
+                    ],
+                  );
+                } else if (snapshot.connectionState == ConnectionState.done) {
+                  Clipboard.setData(ClipboardData(text: snapshot.data.link));
+                  return Text("URL copied to clipboard!");
+                } else {
+                  return Text("Connection state: ${snapshot.connectionState}");
+                }
+              })),
     );
   }
 }
