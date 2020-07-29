4c303a5f0ce8465ab611f758d33eb753e2ffce90
James Harvey <jmsharvey771@gmail.com>
2020-07-29 16:38:14 +0100

    Added API key example file

---
+++ lib/components/getImgurApiKey-EXAMPLE.dart
@@ -0,0 +1,8 @@
+// Instructions:
+// 1. Create an API key at https://api.imgur.com/oauth2/addclient (I used the anonymous auth type)
+// 2. Replace the example text in line 7 with your API key
+// 3. Rename this file to getImgurApiKey.dart
+
+String getImgurApiKey() {
+  return "REPLACE THIS TEXT WITH YOUR API KEY"
+}
