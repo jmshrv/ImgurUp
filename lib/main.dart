6b7eab49cd61f442a9618f2f1d747cf1b1268474
UnicornsOnLSD <44349936+UnicornsOnLSD@users.noreply.github.com>
2020-08-01 15:38:07 +0100

    Made imgur uploader its own widget, moved image picker away from MainScreen to remove the need for Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import 'screens/MainScreen.dart';
 import 'screens/UploadingScreen.dart';
 
-import 'models/ImagePickerProvider.dart';
-
 void main() {
   runApp(
-    MultiProvider(
-      providers: [
-        Provider<ImagePickerProvider>(
-          create: (_) => ImagePickerProvider(),
-        )
-      ],
-      child: ImgurUp(),
-    ),
+    ImgurUp(),
   );
 }
 
--- lib/screens/UploadingScreen.dart
+++ lib/screens/UploadingScreen.dart
@@ -1,72 +1,16 @@
-import 'dart:io';
-import 'dart:convert';
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-import 'package:imgur/imgur.dart' as imgur;
 
-import '../models/ImagePickerProvider.dart';
-import '../components/getImgurApiKey.dart';
+import '../components/ImgurUploaderWidget.dart';
 
-class UploadingScreen extends StatefulWidget {
+class UploadingScreen extends StatelessWidget {
   const UploadingScreen({Key key}) : super(key: key);
-
-  @override
-  _UploadingScreenState createState() => _UploadingScreenState();
-}
-
-class _UploadingScreenState extends State<UploadingScreen> {
-  Future uploadImageFuture;
-
-  Future<imgur.Image> _uploadImageFromProvider() async {
-    ImagePickerProvider imagePickerProvider =
-        Provider.of<ImagePickerProvider>(context, listen: false);
-
-    Uint8List imageBinary = await imagePickerProvider.chosenImage.readAsBytes();
-    String imageBase64 = base64Encode(imageBinary);
-
-    var imgurClient =
-        imgur.Imgur(imgur.Authentication.fromClientId(getImgurApiKey()));
-
-    imgur.Image uploadedImage =
-        await imgurClient.image.uploadImage(imageBase64: imageBase64);
-
-    return uploadedImage;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    uploadImageFuture = _uploadImageFromProvider();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: FutureBuilder(
-              future: uploadImageFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text("Uploading Image..."),
-                      Padding(padding: EdgeInsets.symmetric(vertical: 8)),
-                      CircularProgressIndicator(),
-                    ],
-                  );
-                } else if (snapshot.connectionState == ConnectionState.done) {
-                  Clipboard.setData(ClipboardData(text: snapshot.data.link));
-                  return Text("URL copied to clipboard!");
-                } else {
-                  return Text("Connection state: ${snapshot.connectionState}");
-                }
-              })),
+        child: ImgurUploaderWidget(),
+      ),
     );
   }
 }
