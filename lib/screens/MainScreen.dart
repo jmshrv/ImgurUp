119da3533e5285708f2e6b730273716b3900053f
UnicornsOnLSD <44349936+UnicornsOnLSD@users.noreply.github.com>
2020-08-02 21:42:22 +0100

    Added camera button (and redesigned MainScreen logic)

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -3,42 +3,81 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   const MainScreen({Key key}) : super(key: key);
 
-  double _calculateButtonSize(BuildContext context) {
-    // We can't just use the screen width to resize the button because if the screen is landscape, the button won't look right.
-    // This function needs the context because MediaQuery needs it.
-
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-
-    if (height >= width) {
-      return width * 0.9;
-    } else {
-      return height * 0.9;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Semantics(
-          button: true,
-          label: "Upload",
-          child: Container(
-            width: _calculateButtonSize(context),
-            // No need to calculate height since for this widget, width will always be equal to height.
-            child: FittedBox(
-              child: FloatingActionButton(
-                  child: Icon(
-                    Icons.file_upload,
-                    color: Colors.white,
+      body: Builder(builder: (context) {
+        if (MediaQuery.of(context).size.width >
+            MediaQuery.of(context).size.height) {
+          // If the screen is landscape
+          return SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Container(
+                    child: FractionallySizedBox(
+                      widthFactor: 0.9,
+                      child: FittedBox(
+                        child: FloatingActionButton(
+                            child: Icon(Icons.camera_alt), onPressed: null),
+                      ),
+                    ),
+                    alignment: Alignment.center,
                   ),
-                  onPressed: () =>
-                      Navigator.of(context).pushReplacementNamed("/uploading")),
+                ),
+                Expanded(
+                  child: Container(
+                    child: FractionallySizedBox(
+                      widthFactor: 0.9,
+                      child: FittedBox(
+                        child: FloatingActionButton(
+                            child: Icon(Icons.folder), onPressed: null),
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ),
-      ),
+          );
+        } else {
+          // If the screen is portrait
+          return SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Container(
+                    child: FractionallySizedBox(
+                      heightFactor: 0.9,
+                      child: FittedBox(
+                        child: FloatingActionButton(
+                            child: Icon(Icons.camera_alt), onPressed: null),
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: FractionallySizedBox(
+                      heightFactor: 0.9,
+                      child: FittedBox(
+                        child: FloatingActionButton(
+                            child: Icon(Icons.folder), onPressed: null),
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+      }),
     );
   }
 }
